dc65fbfebb235650d94c5499d74158a1a8b1cb7e
Achsan <achsanh@gmail.com>
2024-08-19 04:51:33 -0400

    🗃️ fix: change code column data type

---
+++ db/migration/000005_alter_code_data_type.up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE province
+ALTER COLUMN code TYPE varchar (18);
+ALTER TABLE village
+ALTER COLUMN code TYPE varchar (18);
+ALTER TABLE district
+ALTER COLUMN code TYPE varchar (18);
+ALTER TABLE city
+ALTER COLUMN code TYPE varchar (18);
