1ed519156efd73302f2d0a2322ef95b44b4968d6
Achsan <achsanh@gmail.com>
2024-08-20 10:35:22 -0400

    🗃️🏷️ fix(province): update `array` data type

--- internal/entity/province_entity.go
+++ internal/entity/province_entity.go
@@ -1,10 +1,12 @@
 package entity
 
+import "github.com/lib/pq"
+
 type Province struct {
-	ID          int    `gorm:"column:id;primaryKey;autoIncrement:false"`
-	Code        string `gorm:"column:code;size:18"`
-	Name        string `gorm:"column:name"`
-	PostalCodes []int  `gorm:"column:postal_codes;type:int4[]"`
+	ID          int           `gorm:"column:id;primaryKey;autoIncrement:false"`
+	Code        string        `gorm:"column:code;size:18"`
+	Name        string        `gorm:"column:name"`
+	PostalCodes pq.Int64Array `gorm:"column:postal_codes;type:int4[]"`
 }
 
 func (p *Province) TableName() string {
--- internal/model/province_model.go
+++ internal/model/province_model.go
@@ -1,10 +1,12 @@
 package model
 
+import "github.com/lib/pq"
+
 type ProvinceResponse struct {
-	ID          int    `json:"id"`
-	Code        string `json:"code"`
-	Name        string `json:"name"`
-	PostalCodes []int  `json:"postal_codes"`
+	ID          int           `json:"id"`
+	Code        string        `json:"code"`
+	Name        string        `json:"name"`
+	PostalCodes pq.Int64Array `json:"postal_codes"`
 }
 
 type ListProvinceRequest struct {
