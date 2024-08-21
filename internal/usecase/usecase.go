8f50380d9f466ffa23417a2ec9bc04d0c0339487
Achsan <achsanh@gmail.com>
2024-08-21 03:06:54 -0400

    🩹fix: make usecase generic

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -1,12 +1,1 @@
 package usecase
-
-import (
-	"context"
-
-	"github.com/aikuci/go-subdivisions-id/internal/model"
-)
-
-type CruderUseCase[T any] interface {
-	List(ctx context.Context, request *model.ListRequest) ([]T, error)
-	GetByID(ctx context.Context, request *model.GetByIDRequest) (*T, error)
-}
