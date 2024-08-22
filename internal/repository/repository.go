539c4f6a32fe1e97091fc2366f0ce0e5663261bc
Achsan <achsanh@gmail.com>
2024-08-22 03:26:09 -0400

    🗃️ feat(setup): update repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,36 +2,63 @@ package repository
 
 import "gorm.io/gorm"
 
-type Repository[T any] struct {
-	DB *gorm.DB
+type Repository[T any] struct{}
+
+// Retrieve Collections
+func (r *Repository[T]) Find(db *gorm.DB) ([]T, error) {
+	var collections []T
+	if err := db.Find(&collections).Error; err != nil {
+		return nil, err
+	}
+	return collections, nil
+}
+func (r *Repository[T]) FindBy(db *gorm.DB, where map[string]interface{}) ([]T, error) {
+	var collections []T
+	if err := db.Where(where).Find(&collections).Error; err != nil {
+		return nil, err
+	}
+	return collections, nil
+}
+func (r *Repository[T]) FindById(db *gorm.DB, id int) ([]T, error) {
+	return r.FindBy(db, map[string]interface{}{"id": id})
+}
+func (r *Repository[T]) FindByIds(db *gorm.DB, ids []int) ([]T, error) {
+	return r.FindBy(db, map[string]interface{}{"id": ids})
 }
 
-func (r *Repository[T]) Create(db *gorm.DB, entity *T) error {
-	return db.Create(entity).Error
+// Retrieve First Collection
+func (r *Repository[T]) First(db *gorm.DB) (*T, error) {
+	var collection T
+	if err := db.First(&collection).Error; err != nil {
+		return nil, err
+	}
+	return &collection, nil
+}
+func (r *Repository[T]) FirstBy(db *gorm.DB, where map[string]interface{}) (*T, error) {
+	var collection T
+	if err := db.Where(where).First(&collection).Error; err != nil {
+		return nil, err
+	}
+	return &collection, nil
+}
+func (r *Repository[T]) FirstById(db *gorm.DB, id int) (*T, error) {
+	return r.FirstBy(db, map[string]interface{}{"id": id})
 }
 
-func (r *Repository[T]) Update(db *gorm.DB, entity *T) error {
-	return db.Save(entity).Error
-}
-
-func (r *Repository[T]) Delete(db *gorm.DB, entity *T) error {
-	return db.Delete(entity).Error
-}
-
-func (r *Repository[T]) CountById(db *gorm.DB, id any) (int64, error) {
+// Retrieve Meta Collection
+func (r *Repository[T]) CountById(db *gorm.DB, id int) (int64, error) {
 	var total int64
 	err := db.Model(new(T)).Where("id = ?", id).Count(&total).Error
 	return total, err
 }
 
-func (r *Repository[T]) FindById(db *gorm.DB, entity *T, id any) error {
-	return db.Where("id = ?", id).Take(entity).Error
+// Collection Action
+func (r *Repository[T]) Create(db *gorm.DB, entity *T) error {
+	return db.Create(entity).Error
 }
-
-func (r *Repository[T]) FindAll(db *gorm.DB) ([]T, error) {
-	var provinces []T
-	if err := db.Find(&provinces).Error; err != nil {
-		return nil, err
-	}
-	return provinces, nil
+func (r *Repository[T]) Update(db *gorm.DB, entity *T) error {
+	return db.Save(entity).Error
+}
+func (r *Repository[T]) Delete(db *gorm.DB, entity *T) error {
+	return db.Delete(entity).Error
 }
